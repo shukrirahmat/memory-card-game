812db966601dc31cdd2479e4ab57460d0954710e
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-09-02 20:28:55 +0800

    add basic skeleton and CardContainer component

---
+++ src/CardContainer.jsx
@@ -0,0 +1,23 @@
+import './CardContainer.css';
+
+function CardContainer() {
+
+    return (
+        <div className="cardcontainer">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+        </div>
+    )
+}
+
+export default CardContainer;
