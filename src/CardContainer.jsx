9a64b9980b0bbae598f7e162a138ecd9e9c161f4
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-09-03 01:24:44 +0800

    add useEffect that fetches sprites

--- src/CardContainer.jsx
+++ src/CardContainer.jsx
@@ -1,23 +1,37 @@
-import './CardContainer.css';
+import "./CardContainer.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function CardContainer() {
+  const [imageSources, setImageSources] = useState([]);
 
-    return (
-        <div className="cardcontainer">
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-        </div>
-    )
+  useEffect(() => {
+    const sources = [];
+    const ids = Array(12)
+      .fill()
+      .map((_, index) => index + 1);
+  
+    const promises = ids.map((id) =>
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+        mode: "cors",
+      }).then((response) => response.json())
+    );
+    Promise.all(promises).then((datas) => {
+      datas.forEach((data) => sources.push(data));
+      setImageSources(sources);
+    });
+  }, []);
+
+  return (
+    <div className="cardcontainer">
+      {imageSources.map((source, index) => {
+        return (
+        <div key={index} className="card">
+            <img src={source.sprites.versions["generation-i"]["red-blue"].front_default} alt='name'></img>
+        </div>);
+      })}
+    </div>
+  );
 }
 
 export default CardContainer;
